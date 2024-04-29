232871468f01a8313c46f7703d5d1660d8272304
113857_DeMaussionGabriel <113857@tecnicatura.frc.utn.edu.ar>
2024-04-29 20:43:38 -0300

    Clases y TP

---
+++ Clase4/JavaScript.js
@@ -0,0 +1,25 @@
+function mens(texto)
+        {
+            alert('mensaje: ' + texto)
+        };
+
+let objeto = 
+        {
+            id : 1,
+            valor : 'xd',
+            metodo : function mostrar()
+            {
+                console.log(objeto.valor)
+            }
+        }
+
+const arreglo = [1, 'texto en arreglo', true]
+
+for (let i = 0; i < 10; i++)
+{
+    console.log('vuelta ' + i)
+}
+
+document
+
+
